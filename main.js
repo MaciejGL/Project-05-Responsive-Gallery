7f911817534c42aaa8af0d1a4c45f01171a7e139
MaciejGL <m.glowacki01@gmail.com>
2020-04-02 00:16:54 +0200

    Gallery ready to use

---
+++ main.js
@@ -0,0 +1,22 @@
+const searchInput = document.getElementById("search");
+
+const imagesCollection = [...document.getElementsByTagName('a')];
+let searchValue;
+
+function myFunction() {
+    searchInput.value = searchInput.value.toLowerCase();
+    searchValue = searchInput.value
+    // console.log(searchValue)
+    for (let i = 0; i < imagesCollection.length; i++) {
+        let attr = imagesCollection[i].getAttribute('data-title').toLowerCase();
+        if (attr.includes(searchValue)) {
+            imagesCollection[i].style.display = 'block';
+            console.log(imagesCollection[i])
+            console.log(searchValue)
+        } else {
+            imagesCollection[i].style.display = 'none'
+        }
+    }
+}
+
+searchInput.addEventListener("keyup", myFunction);
