4197809eaf0d7c76905bee8229eade226dc0be39
MaciejGL <m.glowacki01@gmail.com>
2020-04-27 14:01:55 +0200

    Add few things to JS

--- main.js
+++ main.js
@@ -4,19 +4,19 @@ const imagesCollection = [...document.getElementsByTagName('a')];
 let searchValue;
 
 function myFunction() {
-    let searchInputValue = searchInput.value.toLowerCase()
+    let searchInputValue = searchInput.value.toLowerCase();
     searchValue = searchInputValue;
 
     for (let i = 0; i < imagesCollection.length; i++) {
         let attr = imagesCollection[i].getAttribute('data-title').toLowerCase();
         if (attr.includes(searchValue)) {
             imagesCollection[i].style.display = 'block';
-            console.log(imagesCollection[i])
-            console.log(searchValue)
+            console.log(imagesCollection[i]);
+            console.log(searchValue);
         } else {
-            imagesCollection[i].style.display = 'none'
+            imagesCollection[i].style.display = 'none';
         }
     }
 }
 
-searchInput.addEventListener("keyup", myFunction)
+searchInput.addEventListener("keyup", myFunction);
